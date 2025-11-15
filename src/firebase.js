99bb268bc8730da54eeaa272ec50180fa993b777
flavcaro <flaviana.caroselli@student.univaq.it>
2025-11-15 13:00:30 +0100

    cript key

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2wy_RD0w5d13nsg2JBbSPtPJGuRsSr3c",
-  authDomain: "sketchandguess-959e6.firebaseapp.com",
-  databaseURL: "https://sketchandguess-959e6-default-rtdb.firebaseio.com",
-  projectId: "sketchandguess-959e6",
-  storageBucket: "sketchandguess-959e6.firebasestorage.app",
-  messagingSenderId: "834259164506",
-  appId: "1:834259164506:web:039f69e7a0542bc099a763",
-  measurementId: "G-VB7R1P3J02"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
