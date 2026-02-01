1cce10683ca8b163ceddcc370a28ac973ebe2d0b
flavcaro <flaviana.caroselli@student.univaq.it>
2026-02-01 23:44:31 +0100

    prove

--- src/components/Home/CustomCreateForm.js
+++ src/components/Home/CustomCreateForm.js
@@ -153,8 +153,8 @@ export default function CustomCreateForm({
       )}
 
       <div className="modal-actions-row">
-        <Button onClick={onCancel} variant="tertiary">✕ Annulla</Button>
         <Button onClick={onCreate} variant="primary">Crea stanza</Button>
+        <Button onClick={onCancel} variant="tertiary">✕ Annulla</Button>
       </div>
     </div>
   );
